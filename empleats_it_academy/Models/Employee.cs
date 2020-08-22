9b2073fb5872a82e8f9279b25de26905e982d825
Ofelia <ofeliacocucci@outlook.com>
2020-08-22 20:15:24 +0200

    Api empleats

---
+++ empleats_it_academy/Models/Employee.cs
@@ -0,0 +1,11 @@
+﻿namespace empleats_it_academy.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Job { get; set; }
+        public float Salary { get; set; }
+    }
+}
---
+++ empleats_it_academy/Models/EmployeeContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace empleats_it_academy.Models
+{
+    public class EmployeeContext : DbContext
+    {
+        public EmployeeContext(DbContextOptions<EmployeeContext> options)
+            : base (options)
+        {
+        }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
